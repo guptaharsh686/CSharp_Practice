114e34429b82e67fda679790b9e998d53f73fa9a
harsh_git <guptaharsh686@gmail.com>
2023-07-03 23:36:20 +0530

    initial commit for Delegate code

---
+++ CSharp_Deligates/PhotoFilters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Deligates
+{
+    public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply Brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply Contrase");
+        }
+
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize photo");
+        }
+    }
+}
---
+++ CSharp_Deligates/PhotoProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Deligates
+{
+    public class PhotoProcessor
+    {
+        public void Process(string path)
+        {
+            var photo = Photo.Load(path);
+
+            var filters = new PhotoFilters();
+            filters.ApplyBrightness(photo);
+            filters.ApplyContrast(photo);
+            filters.Resize(photo);
+
+            photo.Save();
+        }
+    }
+}
