874fccb9661ca7b27491a273deece92723629789
harsh_git <guptaharsh686@gmail.com>
2023-07-07 00:20:39 +0530

    implemented small extension method

---
+++ CSharp_Extension_Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp_Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string paragraph = "This is a long long paragraph blah blah ....";
+
+            var shortParagraph = paragraph.Shorten(5);
+            //Shorten is an extension method
+
+            Console.WriteLine(shortParagraph);
+        }
+    }
+}
---
+++ CSharp_Extension_Methods/StringExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Extension_Methods
+{
+    public static class StringExtension
+    {
+        //extension method must be public static and must have first parameter of type of instance object which is calling it 
+        public static string Shorten(this string str,int noOfWords)
+        {
+            if(noOfWords <= 0)
+            {
+                return "";
+            }
+
+            var words = str.Split(' ');
+            var reqWords = new List<string>();
+
+            for(int i = 0; i < noOfWords; i++)
+            {
+                reqWords.Add(words[i]);
+            }
+
+            string vs = "";
+            foreach (var item in reqWords.ToArray())
+            {
+                vs = vs + item;
+                vs = vs + ' ';
+            }
+
+            return vs;
+        }
+    }
+}
